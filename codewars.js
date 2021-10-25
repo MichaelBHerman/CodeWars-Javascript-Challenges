d46715ba107e9730d46a587b291d24e8e13d0582
MichaelBHerman <michaelbherman@gmail.com>
2021-10-24 19:25:03 -0600

    Additional exercises completed

--- codewars.js
+++ codewars.js
@@ -1,3 +1,4 @@
+// Exercise 1
 
 
 // Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
@@ -32,6 +33,10 @@
 //     }).join('');
 //   }
 
+// Exercise 2
+
+// Write a function that reverses a String.
+
 // function reverseString(string){
 //     let newString = ""
 //     for(i = string.length; i >= 0; i--){
@@ -41,6 +46,8 @@
 //     console.log(newString)
 // }
 // reverseString("paddington")
+
+// Exercise 3
 
 // In this Kata we are passing a number (n) into a function.
 
@@ -63,11 +70,18 @@
 
 // oddOrEven (52)
 
+// Exercise 4
+
+// Write a program to determine a simple boolean value at random.
+
 // function coolOrNot (string){
 //     let randomBool = Math.random() > 0.5 ? true : false;
 //     console.log("Is" + " " + string + " " +  "Cool?" + " " + randomBool)
 // }
 // coolOrNot("Clark")
+
+
+// Exercise 5
 
 // Write function countVowels which returns how many times any vowel [a,e,i,o,u] occurs in a string. Consider only lowercase strings.
 
@@ -88,14 +102,39 @@
 //  We check for null incase there are no matches (ie no vowels), and return 0 in that case.
 
 
-function countVowels (string){
-    let count = string.match(/[aeiou]/gi) 
-    if (count === null){
-        return 0;
-    }
-    else{
-        return count.length
-    }
+// function countVowels (string){
+//     let count = string.match(/[aeiou]/gi) 
+//     if (count === null){
+//         return 0;
+//     }
+//     else{
+//         return count.length
+//     }
+// }
+// let result = countVowels ("paddington")
+// console.log(result)
+
+// Exercise 6
+
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+// Examples:
+// s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
+
+const error_printer = word => {
+    const regex = /[a-m]/g
+    const proper_num_of_matches = ((word || '').match(regex) || []).length
+    return `${word.length - proper_num_of_matches}/${word.length}`
 }
-let result = countVowels ("paddington")
-console.log(result)
